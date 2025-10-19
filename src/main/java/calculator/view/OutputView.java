ce630fb9d6994d71b7198a8490fa39f0de5a2fc6
gumguard <gawonlee0712@gmail.com>
2025-10-20 01:02:51 +0900

    style(view): OutputView message 공백 추가

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,7 +1,7 @@
 package calculator.view;
 
 public class OutputView {
-    private static final String OUTPUT_INSTRUCTION = "결과: ";
+    private static final String OUTPUT_INSTRUCTION = "결과 : ";
 
     public void printResult(int result) {
         System.out.print(OUTPUT_INSTRUCTION + result);
