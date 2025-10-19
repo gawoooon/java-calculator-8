cd86726d47330cd45299d7028394c90fd7ad07dc
gumguard <gawonlee0712@gmail.com>
2025-10-19 21:56:24 +0900

    feat(validation): 정수 범위 초과 검증 에러 추가

--- src/main/java/calculator/constant/ErrorMessage.java
+++ src/main/java/calculator/constant/ErrorMessage.java
@@ -2,7 +2,8 @@ package calculator.constant;
 
 public enum ErrorMessage {
     NOT_A_NUMBER("입력된 문자열은 숫자 형식이어야 합니다: "),
-    NEGATIVE_NUMBER("음수는 입력할 수 없습니다: ");
+    NEGATIVE_NUMBER("음수는 입력할 수 없습니다: "),
+    OUT_OF_INTEGER_RANGE("표현 가능한 정수 범위를 벗어났습니다: ");
 
     private final String message;
 
