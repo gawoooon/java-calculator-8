c48a5efb131a03799f262ca0be4fb24461ece9e2
gumguard <gawonlee0712@gmail.com>
2025-10-19 19:50:25 +0900

    feat(constant): ErrorMessage enum 구현 및 PositiveNumber 관련 에러 추가

---
+++ src/main/java/calculator/constant/ErrorMessage.java
@@ -0,0 +1,20 @@
+package calculator.constant;
+
+public enum ErrorMessage {
+    NOT_A_NUMBER("입력된 문자열은 숫자 형식이어야 합니다: "),
+    NEGATIVE_NUMBER("음수는 입력할 수 없습니다: ");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getMessage(Object value) {
+        return message + value;
+    }
+}
