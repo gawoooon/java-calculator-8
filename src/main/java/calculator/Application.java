3e2fe99e233fe704b16511d94e59a91ebcd2da3f
gumguard <gawonlee0712@gmail.com>
2025-10-20 01:29:33 +0900

    feat(application): Application 코드 작성 및 Controller 의존성 주입

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.controller.StringCalculatorController;
+import calculator.model.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculator stringCalculator = new StringCalculator();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        StringCalculatorController controller = new StringCalculatorController(stringCalculator, inputView, outputView);
+        controller.run();
     }
 }
